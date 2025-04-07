ba976f76502d520e0c076307233454ec32dafda0
Bama-S <chorse@gmail.com>
2025-04-07 16:13:17 +0530

    initial commit of mlflow-iris

---
+++ src/train.py
@@ -0,0 +1,34 @@
+# src/train.py
+import os
+import mlflow
+import mlflow.sklearn
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Set MLflow to use the local folder
+mlflow.set_tracking_uri("file://" + os.path.abspath("mlruns"))
+mlflow.set_experiment("Iris_Classifier")
+
+# Load Iris dataset
+iris = datasets.load_iris()
+X_train, X_test, y_train, y_test = train_test_split(
+    iris.data, iris.target, test_size=0.3, random_state=42
+)
+
+
+
+with mlflow.start_run():
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(X_train, y_train)
+
+    predictions = clf.predict(X_test)
+    acc = accuracy_score(y_test, predictions)
+
+    # Log model
+    mlflow.sklearn.log_model(clf, "model")
+    # Log metric
+    mlflow.log_metric("accuracy", acc)
+
+    print(f"Model saved in run {mlflow.active_run().info.run_uuid}")
